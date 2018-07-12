28a3ada97124702ffde9c4c2bc168de66ed424cc
SophieAu <sophieau@users.github.noreply.com>
2018-07-12 18:23:50 +0200

    Add pypi config files

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name         = 'lifester',
+    version      = '1.0.0',
+    author       = 'Sophie Au',
+    author_email = 'some.person@web.de',
+    license      = 'MIT',
+    description  = 'very minute of your life. On the command line.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords     = ['command-line-tool', 'productivity'],
+    url          = 'https://github.com/sophieau/lifester',
+
+    packages     = ['lifester'],
+    zip_safe     = False,
+    entry_points = {
+        'console_scripts': ['lifester=lifester.__main__:main'],
+    },
+    python_requires  = '>=3.6',
+    install_requires = [
+        'tabulate >= 0.8.2',
+    ]
+)
